f1f70895247ba2b43432c8ef3152cc35777f2805
G9000 <leo.caesar@live.com>
2022-10-01 16:19:03 +0800

    init

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,22 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { NhostClient, NhostNextProvider } from "@nhost/nextjs";
+import { NhostApolloProvider } from "@nhost/react-apollo";
+
+const nhost = new NhostClient({
+  subdomain: process.env.NEXT_PUBLIC_NHOST_SUBDOMAIN || "",
+  region: process.env.NEXT_PUBLIC_NHOST_REGION || "",
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const { nhostSession } = pageProps as any;
+  return (
+    <NhostNextProvider nhost={nhost} initial={nhostSession}>
+      <NhostApolloProvider nhost={nhost}>
+        <Component {...pageProps} />
+      </NhostApolloProvider>
+    </NhostNextProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
