dacf42a7bc80de3c12e882213c9249ee2eee0be3
G9000 <leo.caesar@live.com>
2022-10-01 18:37:48 +0800

    cleanup

--- components/auth/AuthenticationBox.tsx
+++ components/auth/AuthenticationBox.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { useRouter } from "next/router";
 import { useSignInEmailPassword, useSignUpEmailPassword } from "@nhost/nextjs";
-import Link from "next/link";
 
 enum AuthTabs {
   SIGN_IN = "Sign In",
